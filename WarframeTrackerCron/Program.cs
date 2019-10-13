4e30adbdd4ecf6f2667257d3c8908cbc4b06df7b
Alex Tiller <StrutTower@gmail.com>
2019-10-13 13:58:55 -0700

    Fixed missing program.cs

---
+++ WarframeTrackerCron/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using TowerSoft.Repository;
+using WarframeTrackerLib.Domain;
+using WarframeTrackerLib.Repository;
+using WarframeTrackerLib.Utilities;
+
+namespace WarframeTrackerCron {
+    class Program {
+        static void Main(string[] args) {
+#if DEBUG
+
+#endif
+            bool validArgumentsFound = false;
+            Dictionary<string, AppTask> tasks = AppTask.GetTasks();
+            if (args.SafeAny()) {
+                foreach (var arg in args) {
+                    if (tasks.ContainsKey(arg)) {
+                        validArgumentsFound = true;
+                        AppTask task = tasks[arg];
+                        IAppTask taskObject = (IAppTask)Activator.CreateInstance(task.Type);
+                        taskObject.StartTask();
+                    }
+                }
+            }
+
+            if (!validArgumentsFound) {
+                DisplayHelpMessage(tasks);
+                Environment.Exit(160);
+            }
+        }
+
+        private static void DisplayHelpMessage(Dictionary<string, AppTask> tasks) {
+            Console.WriteLine("No valid arguments found. Valid arguments are:");
+            Console.WriteLine();
+
+            foreach (var task in tasks) {
+                Console.WriteLine("  " + task.Key);
+                Console.WriteLine("        " + task.Value.Description);
+                Console.WriteLine();
+            }
+        }
+    }
+}
